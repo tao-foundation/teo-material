63303ffa9796cd14696ea2701336f7654f982f1d
trustfarm-vr2 <cpplover@trustfarm.net>
2018-06-27 21:53:38 +0900

    ethminer used and bat for testnet solo mining

---
+++ ethminer-testver/ethminer-localhost.bat
@@ -0,0 +1,12 @@
+﻿@echo off
+
+rem cd ethminer-0.14d1
+
+setx GPU_FORCE_64BIT_PTR 0
+setx GPU_MAX_HEAP_SIZE 100
+setx GPU_USE_SYNC_OBJECTS 1
+setx GPU_MAX_ALLOC_PERCENT 100
+setx GPU_SINGLE_ALLOC_PERCENT 100
+ 
+ethminer -U -F 127.0.0.1:8546 --cuda-devices 1
+
